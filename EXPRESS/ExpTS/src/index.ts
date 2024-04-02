8abb4d04d48ba03610d0391bb9e0f44e0cbf6227
Fabricio Guimaraes <fabricio15.guimaraes@gmail.com>
2024-04-01 23:30:57 -0400

    EXPRESS - exercicio 07 done

--- EXPRESS/ExpTS/src/index.ts
+++ EXPRESS/ExpTS/src/index.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response }from "express";
+import express from "express";
 import validateEnv from "./utils/validateEnv";
 import dotenv from "dotenv";
 import router from "./router/router";
 // import morgan from "morgan";
+import { engine } from "express-handlebars";
 import logger from "./middlewares/logger";
 
 // configurando env
@@ -15,6 +16,10 @@ const PORT = process.env.PORT ?? 3333;
 
 // iniciando app
 const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", `${__dirname}/views`);
 
 // middlewarer de log
 app.use(logger('completo'));
