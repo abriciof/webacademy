cff0aeaab820e895fe4eb7ad64bae92be5cfaf4d
Fabrício Guimarães <fdcg@icomp.ufam.edu.br>
2024-04-16 20:02:27 -0400

    API - Routers

--- API/expApi/src/index.ts
+++ API/expApi/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response} from "express";
 import dotenv from "dotenv";
 import validateEnv from "./utils/validateEnv";
+import router from "./router"
 
 dotenv.config();
 
@@ -9,9 +10,7 @@ validateEnv();
 const app = express();
 const PORT = process.env.PORT ?? 4444;
 
-app.get("/", (req: Request, res: Response) => {
-    res.json({msg: "oiiii"});
-});
+app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
