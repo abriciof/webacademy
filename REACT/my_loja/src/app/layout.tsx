13fe3f260c2dc4306b9ff21a0d682896d5fa67a7
Fabrício Guimarães <fdcg@icomp.ufam.edu.br>
2024-05-07 21:03:54 -0400

    REACT - hook usePathName

--- REACT/my_loja/src/app/layout.tsx
+++ REACT/my_loja/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BootstrapClient from "./components/BootstrapClient";
 import Navbar from "./components/Navbar";
+import { usePathname } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "WA Loja",
@@ -12,13 +13,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body>
         <Navbar />
 
         {children}
-        
+
         <BootstrapClient />
         </body>
     </html>
